7c55bf5ee3bf85a20a22d953999625e2cea80035
tvinnie <tvinnie77@gmail.com>
2021-12-05 22:47:04 +0300

    Laravel ChatApp

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # ChatApp
-Chat App using: Laravel , Vuejs and Pusher for notifications
+Chat App using: 
+
+	Laravel (Backend PHP Framework)
+	Laravel Sanctum(Authentication Package)
+	Jetstream Inertia(Front/Back end user authentication scaffolding)
+	Vue.js(FrontEnd Javascript Framework)
+	Pusher(Real-time web-socket connects)
---
+++ resources/js/Pages/Chat/container.vue
@@ -0,0 +1,99 @@
+<template>
+    <app-layout title="Dashboard">
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+               <room-selection
+                v-if="currentRoom.id"
+                :rooms="chatRooms"
+                :currentRoom="currentRoom"
+                v-on:roomchanged="setRoom($event)"
+               />
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                  <message-container :messages="messages"/>
+                  <input-message 
+                    :room="currentRoom"
+                    v-on:messagesent="getMessages()"
+                  />
+                </div>
+            </div>
+        </div>
+    </app-layout>
+</template>
+
+<script>
+    import { defineComponent } from 'vue'
+    import AppLayout from '@/Layouts/AppLayout.vue'
+    import MessageContainer from './messageContainer.vue'
+    import InputMessage from './inputMessage.vue'
+    import RoomSelection from './roomSelection.vue'
+
+    export default defineComponent({
+        components: {
+            AppLayout,
+            MessageContainer,
+            InputMessage,
+            RoomSelection
+        },
+        data: function () {
+            return {
+                chatRooms:[],
+                currentRoom:[],
+                messages:[]
+            }
+        },
+        watch:{
+            currentRoom(val, oldVal) {
+                if(oldVal.id){
+                    this.disconnect(oldVal);
+                }
+                this.connect();
+            }
+        },
+        methods:{
+            connect(){
+                if(this.currentRoom.id) {
+                    let vm = this;
+                    this.getMessages();
+                    window.Echo.private("chat." + this.currentRoom.id)
+                    .listen('.message.new', e => {
+                        vm.getMessages();
+                    })
+                }
+            },
+            disconnect() {
+                window.Echo.leave("chat." + room.id);
+            },
+            getRooms() {
+                axios.get('/chat/rooms')
+                .then( response => {
+                    this.chatRooms = response.data;
+                    this.setRoom( response.data[0] );
+                })
+                .catch(error => {
+                console.log('error');
+                })
+            },
+            setRoom(room) {
+                this.currentRoom = room;
+                // this.getMessages();
+            },
+            getMessages(){
+                axios.get('/chat/room/' + this.currentRoom.id + '/messages') 
+                .then( response => {
+                    this.messages = response.data;
+                })
+                .catch( error => {
+                    console.log(error);
+                })
+            }
+        },
+        created() {
+            this.getRooms();
+        }
+    })
+</script>
